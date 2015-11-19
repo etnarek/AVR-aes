0dde83c84d774f7342bdc4db9ce9d1fe0427229f
Romain Fontaine <romain-fontaine@outlook.com>
2015-11-19 11:42:37 +0100

    rename avr file playing with matrix matrix

--- matrix.h
+++ matrix.h
@@ -1,8 +1,16 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-//void swap(unsigned char *a, unsigned char *b);
+
+extern unsigned char subByte(const unsigned char byte);
+extern unsigned char getRCON(const unsigned char byte);
+extern unsigned char multBy2(const unsigned char byte);
+extern unsigned char multBy3(const unsigned char byte);
+extern unsigned char getMatrix(const unsigned char pos);
+extern unsigned char setMatrix(const unsigned char pos, const unsigned char val);
 extern void transpose(unsigned char matrix[16]);
 extern void copy(const unsigned char source[16], unsigned char dest[16]);
+extern unsigned char matrix[16];
+extern unsigned char roundKey[16];
 
 #endif /*MATRIX_H*/
