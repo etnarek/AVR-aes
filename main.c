e2c8f6933ff7ade9dbed8e69ae5c246c0d362849
Romain Fontaine <romain-fontaine@outlook.com>
2015-12-02 15:18:19 +0100

    full main in assembly

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 const unsigned char key[16] = {0X00,0X01,0X02,0X03,0X04,0X05,0X06,0X07,0X08,0X09,0X0a,0X0b,0X0c,0X0d,0X0e,0X0f};
 
-extern int main(void);
 //{
 //    USART_init();
 //    USART_send('*');
