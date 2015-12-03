784f480ef153bdeb7dbbc3c820330252c002e769
Romain Fontaine <romain-fontaine@outlook.com>
2015-12-03 22:21:47 +0100

    did somes clean up

--- test/test.py
+++ test/test.py
@@ -3,15 +3,13 @@ import codecs
 from time import sleep
 
 ser = serial.Serial("/dev/ttyACM1", 9600)
-print(ser.read())
+print(ser.read().decode("utf-8"))
 
 text = "00112233445566778899AABBCCDDEEFF\n"
 text = text[:32]
 text_ascii = codecs.decode(text, "hex")
 
 ser.write(text_ascii)
-while 1:
-    ret = codecs.encode(ser.read(16), "hex").decode("utf-8").upper()
-    print(ret)
+ret = codecs.encode(ser.read(16), "hex").decode("utf-8").upper()
 assert ret == "69C4E0D86A7B0430D8CDB78070B4C55A", "%s is not equal with 69C4E0D86A7B0430D8CDB78070B4C55A" % ret
 print("Tout est OK: " + ret)
